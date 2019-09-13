9f6ed21a9471725578d9a92bd8fd0c8553de0982
zubata <50323296+zubata@users.noreply.github.com>
2019-09-13 17:40:38 +0300

    fix

--- homework02/src/main/java/ru/otus/database/QuestionDB.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.database;
-
-import java.util.HashMap;
-
-public interface QuestionDB {
-    HashMap<String, String> getQuestions();
-    void setQuestionAndAnswer(String question, String answer);
-}
--- src/main/java/ru/otus/service/PollService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.service;
-
-import java.io.IOException;
-
-public interface PollService {
-    void testing() throws IOException;
-}
